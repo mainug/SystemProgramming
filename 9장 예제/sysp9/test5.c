e3c9aaed953f6dca835b8164051561a5bdc71808
박민욱 <48702167+mainug@users.noreply.github.com>
2024-06-03 14:52:40 +0900

    Add files via upload

---
+++ 9장 예제/sysp9/test5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "student.h"
+
+/* 텍스트파일에서학생정보를읽어프린트한다. */
+int main(int argc, char *argv[])
+{
+    struct student record;
+    FILE *fp;
+    if (argc != 2)
+    {
+        fprintf(stderr, "사용법: %s 파일이름\n", argv[0]);
+        return 1;
+    }
+    fp = fopen(argv[1], "r");
+    printf("%-9s %-7s %-4s\n", "학번", "이름", "점수");
+    while (fscanf(fp, "%d %s %d", &record.id, record.name, &record.score) == 3)
+        printf("%d %s %d\n", record.id, record.name, record.score);
+    fclose(fp);
+    return 0;
+}
